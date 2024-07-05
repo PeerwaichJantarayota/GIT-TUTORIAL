96707b3f65d028ec6d71f1017cf497d51be5109e
Peerawich Jantarayota <146768049+PeerwaichJantarayota@users.noreply.github.com>
2024-07-05 17:12:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
     echo "<h1>my repository</h1>" 
     echo "<h1>my IGGY narak</h1>" 
     echo "<h1>my name is Boz</h1>" 
-
+    echo "<h1>my repository</h1>" 
+    echo "<h1>my IGGY narak</h1>" 
+    echo "<h1>my name is Boz</h1>" 
 ?>
